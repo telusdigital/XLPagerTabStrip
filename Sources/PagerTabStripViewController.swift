9a5dedc74c3476fdde1a5052b63a245f202ed33f
vknez <vknezevic@nascentdigital.com>
2018-01-18 18:46:29 -0500

    PagerTabStripViewController
    Updated code so that initially only the selected tab’s view controller is loaded.

--- Sources/PagerTabStripViewController.swift
+++ Sources/PagerTabStripViewController.swift
@@ -103,12 +103,6 @@ open class PagerTabStripViewController: UIViewController, UIScrollViewDelegate {
         containerView.showsHorizontalScrollIndicator = false
         containerView.isPagingEnabled = true
         reloadViewControllers()
-
-        let childController = viewControllers[currentIndex]
-        addChildViewController(childController)
-        childController.view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        containerView.addSubview(childController.view)
-        childController.didMove(toParentViewController: self)
     }
 
     open override func viewWillAppear(_ animated: Bool) {
